36cb85156d49517a6b1b90509ee9c113da6df257
devil.tamachan <devil.tamachan@gmail.com>
2017-03-28 01:30:56 +0900

    [追加] メタセコスクリプト各種

---
+++ MetaseqScripts/selectVertexBySelectLine.py
@@ -0,0 +1,24 @@
+doc = MQSystem.getDocument()
+for o in doc.object:
+  if o == None:
+    continue
+  idxObj = doc.getObjectIndex(o)
+  for f in o.face:
+    idxFace = o.getFaceIndexFromUniqueID(f.id)
+    if f.numVertex<2:
+      break
+    
+    for vii in range(f.numVertex):
+      if doc.isSelectLine(idxObj, idxFace, vii)==1:
+        vi = f.index[vii]
+        doc.addSelectVertex(idxObj, vi)
+        MQSystem.println("obj: "+str(idxObj)+", face: "+str(idxFace)+", vidx: "+str(vi))
+        if vii==f.numVertex-1:
+          vi = f.index[0]
+          doc.addSelectVertex(idxObj, vi)
+          MQSystem.println("obj: "+str(idxObj)+", face: "+str(idxFace)+", vidx: "+str(vi))
+        else:
+          vi = f.index[vii+1]
+          doc.addSelectVertex(idxObj, vi)
+          MQSystem.println("obj: "+str(idxObj)+", face: "+str(idxFace)+", vidx: "+str(vi))
+        MQSystem.println("")
---
+++ MetaseqScripts/xEq0.py
@@ -0,0 +1,41 @@
+def setX0(v):
+  p1 = v.getPos()
+  p1.x = 0
+  v.setPos(p1)
+
+
+doc = MQSystem.getDocument()
+for o in doc.object:
+  if o == None:
+    continue
+  idxObj = doc.getObjectIndex(o)
+  for f in o.face:
+    idxFace = o.getFaceIndexFromUniqueID(f.id)
+    if f.numVertex<2:
+      break
+    
+    for li in range(f.numVertex):
+      vi = f.index[li]
+      if doc.isSelectVertex(idxObj, vi)==1:
+        v = o.vertex[vi]
+        setX0(v)
+        MQSystem.println("obj: "+str(idxObj)+", vidx: "+str(vi))
+      if doc.isSelectLine(idxObj, idxFace, li)==1:
+        vi = f.index[li]
+        doc.addSelectVertex(idxObj, vi)
+        v = o.vertex[vi]
+        setX0(v)
+        MQSystem.println("obj: "+str(idxObj)+", face: "+str(idxFace)+", vidx: "+str(vi))
+        if li==f.numVertex-1:
+          vi = f.index[0]
+          doc.addSelectVertex(idxObj, vi)
+          v = o.vertex[vi]
+          setX0(v)
+          MQSystem.println("obj: "+str(idxObj)+", face: "+str(idxFace)+", vidx: "+str(vi))
+        else:
+          vi = f.index[li+1]
+          doc.addSelectVertex(idxObj, vi)
+          v = o.vertex[vi]
+          setX0(v)
+          MQSystem.println("obj: "+str(idxObj)+", face: "+str(idxFace)+", vidx: "+str(vi))
+        MQSystem.println("")
